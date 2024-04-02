5dc0938471dac6a2df369ce6162fb810b386aae9
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-04-02 19:37:42 +0100

    Done: Bonding Curve contract documentation

--- smart contract/contracts/XexadonBondingCurve.sol
+++ smart contract/contracts/XexadonBondingCurve.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
+/// @title BondingCurve
+/// @author NatX
+/// @notice this contract is used to determine the price of an NFT 
+/// @dev the contract implements the constant product market maker
 contract BondingCurve {
     
     constructor() {}
 
-    function getBuyPrice(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256, uint256, uint256) {
-        uint256 amountIn;
-        uint256 newReserve0;
-        uint256 newReserve1;
-
+    /// @notice determines the price of the NFTs a user wants to buy
+    /// @dev the XYK curve is used to determine how much is to be paid for the number of NFTs specified
+    /// @param tokenLength the number of NFTs the user wants to buy
+    /// @param reserve0 the reserve NFT balance from the pair pool
+    /// @param reserve1 the reserve ETH balance from the pair pool
+    /// @return amountIn the price for the NFTs
+    /// @return newReserve0 the new NFT reserve to be updated in the pair pool
+    /// @return newReserve1 the new ETH reserve to be updated in the pair pool
+    function getBuyPrice(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256 amountIn, uint256 newReserve0, uint256 newReserve1) {
         amountIn = ((reserve1 * tokenLength) / (reserve0 - tokenLength));
 
         newReserve0 = reserve0 - tokenLength;
@@ -18,11 +26,15 @@ contract BondingCurve {
         return(amountIn, newReserve0, newReserve1);
     }
 
-    function getSellAmount(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256, uint256, uint256) {
-        uint256 amountOut;
-        uint256 newReserve0;
-        uint256 newReserve1;
-
+    /// @notice determines the amount of ETH a user gets when they sell their NFTs
+    /// @dev the XYK curve is used to determine how much a user gets for selling their NFTs
+    /// @param tokenLength the number of NFTs the user wants to sell
+    /// @param reserve0 the reserve NFT balance from the pair pool
+    /// @param reserve1 the reserve ETH balance from the pair pool
+    /// @return amountOut the amount the user gets for their NFTs
+    /// @return newReserve0 the new NFT reserve to be updated in the pair pool
+    /// @return newReserve1 the new ETH reserve to be updated in the pair pool
+    function getSellAmount(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256 amountOut, uint256 newReserve0, uint256 newReserve1) {
         amountOut = ((reserve1 * tokenLength) / (reserve0 + tokenLength));
 
         newReserve0 = reserve0 + tokenLength;
--- smart contract/interfaces/IXexadonBondCurve.sol
+++ smart contract/interfaces/IXexadonBondCurve.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IXexadonBondCurve {
-    function getBuyPrice(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256, uint256, uint256);
-    function getSellAmount(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256, uint256, uint256);
+    function getBuyPrice(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256 amountIn, uint256 newReserve0, uint256 newReserve1);
+    function getSellAmount(uint256 tokenLength, uint256 reserve0, uint256 reserve1) external pure returns(uint256 amountOut, uint256 newReserve0, uint256 newReserve1);
 }
