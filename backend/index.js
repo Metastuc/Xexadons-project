fdd336d9b90f45f46d5a98d4ba338276995be69c
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-04-05 14:18:57 +0100

    Done: get userCollections endpoint.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const cors = require('cors');
 const path = require('path');
+const axios = require('axios');
 require('dotenv').config();
-
-// Import Moralis
-const Moralis = require("moralis").default;
-
-// Import the EvmChain dataType
-const { EvmChain } = require("@moralisweb3/common-evm-utils");
 
 // Add a variable for the api key, address and chain
 const moralisApiKey = process.env.MORALIS_API;
@@ -17,11 +12,13 @@ const admin = require('firebase-admin');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require("./chainquests-firebase-adminsdk-xqkvw-70590c52d4.json");
+const credentialPath = process.env.CREDPATH;
+const serviceAccount = require(credentialPath);
+
+const raribleApiKey = process.env.RARIBLE_APIKEY;
 
 initializeApp({
     credential: cert(serviceAccount),
-    storageBucket: 'chainquests.appspot.com'
 });
 
 const db = getFirestore();
@@ -29,28 +26,59 @@ const db = getFirestore();
 const app = express();
 const port = process.env.PORT || 3300;
 
-// Enable CORS for any origin
 app.use(cors({
-    origin: '*', // Allow requests from any origin
-    credentials: true, // Include if you're using credentials (e.g., cookies, authorization headers)
+  origin: '*',
+  credentials: true,
 }));
 
 app.use(express.json());
 
 const startServer = async () => {
-    await Moralis.start({
-      apiKey: moralisApiKey,
-    });
-
-    app.listen(port, "0.0.0.0", () => {
-      console.log(`Example app listening on port ${port}`);
-    });
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Example app listening on port ${port}`);
+  });
 };
 
 app.get("/", (req, res) => {
-    res.send("Hello Xexadon!");
+  res.send("Hello Xexadon!");
 });
 
+app.get("/getUserCollections", async(req, res) => {
+  const userAddress = req.query.userAddress;
+  const chain = req.query.chain;
+
+  var userCollections = [];
+  const options = {
+    method: 'GET',
+    url: `https://testnet-api.rarible.org/v0.1/collections/byOwner?blockchains=&owner=ETHEREUM%3A${userAddress}`,
+    headers: {
+      accept: 'application/json',
+      'X-API-KEY': raribleApiKey
+    }
+  };
+
+  try {
+    const response = await axios(options);
+    const collections = response.data.collections;
+    for (let i = 0; i < collections.length; i++) {
+      // check if type is 721
+      if (collections[i].type == "ERC721" && collections[i].id.includes(chain)) {
+        const collection = {
+          name: collections[i].name,
+          symbol: collections[i].symbol,
+          address: collections[i].id.match(/0x[a-fA-F0-9]{40}/)[0]
+        }
+        userCollections.push(collection);
+      }
+    }
+    // Return the userCollections array as the response
+    res.status(200).json(userCollections);
+  } catch (error) {
+    // Handle errors
+    console.error("Error fetching user collections:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 
 startServer();
