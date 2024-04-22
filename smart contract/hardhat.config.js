9d9b2478536d035e2515555af1ac02a8ae671d40
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-04-22 13:34:19 +0100

    Done: Polygon and BSC testnets deployments

--- smart contract/hardhat.config.js
+++ smart contract/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    Amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    BSC: {
+      url: "https://bsc-testnet-rpc.publicnode.com",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
 };
