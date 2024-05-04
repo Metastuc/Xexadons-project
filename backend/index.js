19577b89d746121bf63a492e2b7090b7960a4147
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-05-04 16:20:53 +0100

    Done: Updating deployment addresses

--- backend/index.js
+++ backend/index.js
@@ -264,19 +264,21 @@ const ABIs = {
 
 const deploymentAddresses = {
   factory: {
-    80002: "0x259396819269dF5D4B013dc160F81455bF810C37",
+    80002: "0x63f52C7d448cFdd3ED6F8B4Ad92272B1419895b0",
+    97: "0xFa2C0D7AD5bf7259F564380D707cF95683CBe264"
   },
   curve: {
-    80002: "0x5e70ac9386A9fc36682C507Ba78D3Cb916e2d0a1",
+    80002: "0x471367B20F644E058F7092a34b2d2Ea90B26BB0d",
+    97: "0xCc045dCb5C6FEf4273B498e0c364f760F0415997"
   },
   router: {
-    mainnets: {
-        polygon: "0x.."
-    },
-    testnets: {
-        polygon: "0x6A2fE51B2793879FC3d6C4bD54ec6337Eb7de2B4",
-    }
-}
+    80002: "0x7937b7787E1236685162EedE657b9d631025F2Fb",
+    97: "0x5C67Bf96A7508bFf7a8B3bfe53E6108066F7b41E"
+  },
+  xexadon: {
+    80002: "0x64dCb39317940d74b711eCE72595b6a80D37B8ad",
+    97: "0x5f74e9D1EDA4fcd81B2Aa9C842eB1EE47561f70d"
+  }
 }
 
 const rpcUrls = {
