9ccfa9ada1462daabdffe97ec8188d82b89e9e51
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2024-06-14 01:32:00 +0100

    Updating endpoints

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -64,3 +64,63 @@ export async function getUserNFTs(chain: number, address: string, userAddress: `
 		throw error;
 	}
 }
+
+export async function getUserPools(chainId: number, userAddress: string): Promise<any> {
+    let URL: string, RESPONSE: any;
+
+    URL = `${API_URL}getUserPools?chainId=${chainId}&userAddress=${userAddress}`;
+
+    try {
+        RESPONSE = await axios.get(URL);
+        console.log(RESPONSE.data);
+        return RESPONSE.data;
+    } catch (error) {
+        console.error(`Error trying to fetch user pools: ${error}`);
+        throw error;
+    }
+}
+
+export async function getUser(chainId: number, userAddress: string): Promise<any> {
+    let URL: string, RESPONSE: any;
+
+    URL = `${API_URL}getUser?chainId=${chainId}&userAddress=${userAddress}`;
+
+    try {
+        RESPONSE = await axios.get(URL);
+        console.log(RESPONSE.data);
+        return RESPONSE.data;
+    } catch (error) {
+        console.error(`Error trying to fetch user data: ${error}`);
+        throw error;
+    }
+}
+
+export async function getUserCollections(chainId: number, userAddress: string): Promise<any> {
+    let URL: string, RESPONSE: any;
+
+    URL = `${API_URL}getUserCollections?chainId=${chainId}&userAddress=${userAddress}`;
+
+    try {
+        RESPONSE = await axios.get(URL);
+        console.log(RESPONSE.data);
+        return RESPONSE.data;
+    } catch (error) {
+        console.error(`Error trying to fetch user collections: ${error}`);
+        throw error;
+    }
+}
+
+export async function getPoolActivity(poolId: string): Promise<any> {
+    let URL: string, RESPONSE: any;
+
+    URL = `${API_URL}getPoolActivity?poolId=${poolId}`;
+
+    try {
+        RESPONSE = await axios.get(URL);
+        console.log(RESPONSE.data);
+        return RESPONSE.data;
+    } catch (error) {
+        console.error(`Error trying to fetch pool activity: ${error}`);
+        throw error;
+    }
+}
